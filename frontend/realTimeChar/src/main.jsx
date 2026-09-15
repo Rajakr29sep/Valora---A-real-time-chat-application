1835a86f35aaea37ec81055f296a9010f728c5ac
Raja kumar <aryankumar29sep@gmail.com>
2026-09-15 19:07:43 +0530

    Update server URL to production endpoint

--- frontend/realTimeChar/src/main.jsx
+++ frontend/realTimeChar/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { store } from './redux/store.js'
-export const serverUrl = "http://localhost:8080";
+export const serverUrl = "https://valora-a-real-time-chat-application.onrender.com";
 createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
